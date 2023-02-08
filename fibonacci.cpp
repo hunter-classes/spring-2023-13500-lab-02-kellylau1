7919f58da1f4d20af6c57675ce922dc38acd5ffd
kellylau <kellylaup@gmail.com>
2023-02-08 13:25:36 -0500

    I created fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  int index, value; 
+  int fib[60];
+  fib[0] = 0;
+  fib[1] = 1;
+  
+  std::cout << fib[0] << std::endl; 
+  std::cout << fib[1] << std::endl; 
+  
+  for(index = 2; index < 60; index++) {
+    value = fib[index-1] + fib[index-2];
+    fib[index] = value;
+    std::cout << value << std::endl; 
+  }
+  
+return 0; 
+}
